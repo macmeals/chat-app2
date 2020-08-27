a7fae99de3564e6df1a53beab772f969f8708613
macmeals <loseworld9731@gmail.com>
2020-08-28 06:47:10 +0900

    ユーザー情報表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
-  def users
-    
+  def edit
   end
+
   def update
     if current_user.update(user_params)
-       redirect_to root_path
+      redirect_to root_path
     else
-       render edit
+      render :edit
     end
-
   end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email)
